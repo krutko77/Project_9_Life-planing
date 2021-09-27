bdbcf5aa8068f73b9314a637c21210082f49e41a
Pavel <krutko7779@gmail.com>
2021-09-27 22:18:59 +0300

    header html, css

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ const path = {
 
 // Если нужно выполнять преобразование файлов в определенном порядке, то используем массив с нужным нам порядком:
 const jsFiles = [
-    srcPath + 'assets/js/lib.js',
+   //  srcPath + 'assets/js/lib.js',
     srcPath + 'assets/js/main.js'
 ]
 
